ab3f1c6083e60c6a84fd578379d9f4754eac06c9
phipno <phipno@gmail.com>
2024-04-24 13:07:01 +0200

    finished ex00

--- python-piscine/ex00/Hello.py
+++ python-piscine/ex00/Hello.py
@@ -1,18 +1,25 @@
-#*--------------------------------------------------------------------------------------*#
-#*                                                                                .|    *#
-#*     $FILENAME__________________________________    /     (__)          |/            *#
-#*                                                          (oo)------/'   ,__,    ,    *#
-#*     By: $AUTHOR________________________________       |  (__)     ||    (oo)_____/   *#
-#*                                                             ||---/||    (__)    ||   *#
-#*     Created: $CREATEDAT_________ by $CREATEDBY_    |/                 ,    ||--w||   *#
-#*                                                  ,,       !              |'          *#
-#*                                                       ,           ,|             |/  *#
-#*----------------8<------------------[ mooooooo ]--------------------------------------*#
 
 ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
+
+#Lists[] are ordered, changeable and allow duplicate values
+ft_list[-1] = "World!"
+
+#Tuple() are ordered, unchangeable and allow duplicate values
+changable_list = list(ft_tuple)
+changable_list[-1] = "France!"
+ft_tuple = tuple(changable_list)
+
+#Set{} unordered, unchangeable* and unindexed
+#set items are unchangeable but you can remove and add
+ft_set.remove("tutu!")
+ft_set.add("Paris!")
+
+#dict{} are used to store data values in key:value pairs
+#are changeable and do not allow duplicates since python 3.7 ordered
+ft_dict["Hello"] = "42Paris!"
 
 #your code here
 
@@ -20,5 +27,3 @@ print(ft_list)
 print(ft_tuple)
 print(ft_set)
 print(ft_dict)
-
-#.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~  EOF  ~._.~"~.__.~"~._.~"~._.~"~._.~"~._.~"~._.~#
